fac01a8e25dd415dc346a7a5e76b3cf5aea76e24
lucassegurola <lucassegurola@gmail.com>
2018-10-27 03:20:51 -0300

    cors origin added to provider

--- src/main/java/com/develop/beer2js/repository/ProviderRepository.java
+++ src/main/java/com/develop/beer2js/repository/ProviderRepository.java
@@ -3,7 +3,10 @@ package com.develop.beer2js.repository;
 import com.develop.beer2js.model.Provider;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+
+@CrossOrigin(origins = "http://localhost:4200")
 @Repository
 public interface ProviderRepository extends JpaRepository<Provider, Long> {
 }
