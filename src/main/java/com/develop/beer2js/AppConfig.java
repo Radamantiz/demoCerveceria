e9ec4da18aa46e360370795c81100def55647d8e
lucassegurola <lucassegurola@gmail.com>
2018-10-30 18:45:04 -0300

    corrected thinks

--- src/main/java/com/develop/beer2js/AppConfig.java
+++ src/main/java/com/develop/beer2js/AppConfig.java
@@ -22,25 +22,3 @@ public class AppConfig extends WebMvcConfigurerAdapter {
                 .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE");
     }
 }
-//    @Bean
-//    public DataSource dataSource() {
-//        return DataSourceBuilder
-//                .create()
-//                .username("uqjusabsufocyc")
-//                .password("247ac26865169cadccd71d4c122ac0ad0cc9ebfd059b36614f412fcd91d650b1")
-//                .url("jdbc:postgresql://uqjusabsufocyc:247ac26865169cadccd71d4c122ac0ad0cc9ebfd059b36614f412fcd91d650b1@ec2-54-225-241-25.compute-1.amazonaws.com:5432/de8bubeq18429i")
-//                .driverClassName("org.postgresql.Driver")
-//                .build();
-  //  }
-    /*
-    @Bean
-    public DataSource dataSource() {
-        return DataSourceBuilder
-                .create()
-                .username("springuser")
-                .password("ThePassword")
-                .url("jdbc:postgresql://localhost:5432/db_beer")
-                .driverClassName("org.postgresql.Driver")
-                .build();
-    }*/
-
