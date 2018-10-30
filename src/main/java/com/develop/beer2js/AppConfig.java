ec5f10af74d1a3eb45f11bb82e089464bdb21467
Lucas <lucas.x.segurola.-ND@disney.com>
2018-10-30 15:56:54 -0300

    cors global

--- src/main/java/com/develop/beer2js/AppConfig.java
+++ src/main/java/com/develop/beer2js/AppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 @Configuration
-@ComponentScan("com.concretepage")
+@ComponentScan("com.develop")
 @EnableWebMvc
 public class AppConfig extends WebMvcConfigurerAdapter {
     @Override
