357343fe926aa71398347c63b762d5ee1eabc1ab
Lucas <lucas.x.segurola.-ND@disney.com>
2018-11-01 17:56:46 -0300

    cors changed

--- src/main/java/com/develop/beer2js/WebConfig.java
+++ src/main/java/com/develop/beer2js/WebConfig.java
@@ -14,6 +14,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedMethods("POST,DELETE,OPTIONS,GET,PUT")
+                .allowedOrigins("*");
     }
 }
